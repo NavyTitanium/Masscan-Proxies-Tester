0688c23ce3799fb9b65c4c9c6148400812e3051a
NavyTitanium <33707347+NavyTitanium@users.noreply.github.com>
2019-01-16 08:44:09 -0500

    Update process.py
    Using text files won't scale, using a DB instead.

--- process.py
+++ process.py
@@ -10,203 +10,224 @@ import hashlib
 import queue
 import logging
 from optparse import OptionParser
-lock = threading.Lock()
+import pyodbc
+import socket
+import struct
+import progressbar
+import time
+from tqdm import *
 
-UA='User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0'
-MD5_SUM=""
+cnxn = pyodbc.connect('DRIVER={MySQL ODBC 8.0 Unicode Driver};SERVER=127.0.0.1;PORT=3306;DATABASE=proxy;USER=root;PASSWORD=somepassword')
+cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+cnxn.setencoding('utf-8')
+
+UA = 'User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0'
+MD5_SUM = ""
+page_body=""
 sentinel = object()
 
-def parse_results(file):
-	logging.info("Reading " + file)
-	f = open(file, "r")
-	z=[]
-	for x in f:
-		if "#" not in x:
-			y=x.split( )
-			if len(y) == 5:
-				port=y[2]
-				ip=y[3]
-				timestamp=y[4]
-				dict={"ip":ip,"port":port,"scanned_time":timestamp}
-				z.append(dict)
 
-	logging.info(str(len(z)) +" proxies loaded from file")
-	return z
+def ip2int(addr):
+    return str(struct.unpack("!I", socket.inet_aton(addr))[0])
 
-def fingerprint(website,TIMEOUT):
-	try:
-		req = urlrequest.Request(website)
-		req.addheaders = [(UA)]
-		response = urlrequest.urlopen(req, timeout=TIMEOUT)
-		m = hashlib.md5()
-		m.update(response.read())
-		md5_hash=m.hexdigest()
-		logging.info("Hash value of the content of " + website + " : " + md5_hash)
-		return md5_hash
-	except Exception as e:
-		logging.error(e)
-		logging.error("Cannot fetch the website used to compare integrity!")
-		exit(0)
 
-def test_proxies(proxy,website,TIMEOUT,ignore):
-	try:
-		req = urlrequest.Request(website)
-		req.set_proxy(proxy, 'http')
-		req.addheaders=[(UA)]
-		response = urlrequest.urlopen(req,timeout=TIMEOUT)
-	except ConnectionRefusedError:
-		return False, "ConnectionRefusedError"
-	except ConnectionResetError:
-		return False, "Connection reset"
-	except http.client.BadStatusLine:
-		return False, "Bad status"
-	except IOError as a:
-		if hasattr(a,'code'):
-			return False,a.code
-		if hasattr(a,'reason'):
-			return False,a.reason
-		else:
-			return False, a
-	except urllib.error.URLError as z:
-		if hasattr(z,'code'):
-			return False,z.code
-		if hasattr(z,'reason'):
-			return False, z.reason
-		else:
-			return False,z
-	else:
-		if ignore is None:
-			return True, str(response.getcode())
+def int2ip(addr):
+    return str(socket.inet_ntoa(struct.pack("!I", addr)))
 
-		m = hashlib.md5()
-		content=response.read()
-		m.update(content)
-		if m.hexdigest() != MD5_SUM:
-			if "login".encode() in content:
-				return False, str(response.getcode()) +" Login required?"
-			else:
-				return False, str(response.getcode()) + " Content altered"
-		else:
-			return True, str(response.getcode()) + " Integrity check OK"
 
-def save(line,file):
-	lock.acquire()
-	mode = 'a' if os.path.exists(file) else 'w'
-	with open(file, mode) as f:
-		f.write(line+'\n')
-	lock.release()
+def update_db_result(proxy, reason):
+    try:
+        cursor = cnxn.cursor()
+        ip, port = proxy.split(":")
+        cursor.execute(
+            "INSERT INTO proxies (ipv4,port,reason) VALUES ('" + ip2int(ip) + "','" + port + "','" + reason + "')")
+        #print("INSERT INTO proxies (ipv4,port,reason) VALUES ('" + ip2int(ip) + "','" + port + "','" + reason + "')")
+        cnxn.commit()
+        # cursor.execute("UPDATE proxies SET reason='"+ reason +"' where ipv4 ='" + ip2int(ip) + "' and port = '" + port + "'")
+    except Exception as ex:
+        logging.exception(ex)
 
-def load_and_remove_dupp(inq,proxies,output_good,output_bad):
-	z = []
-	if os.path.exists(output_good):
-		f1 = open(output_good, "r")
-		for x1 in f1:
-			z.append(x1.split("--")[0])
-		f1.close()
-		logging.info("Loading " + str(len(z)) + " good elements already tested")
+def already_in_db(proxy):
+    try:
+        ip, port = proxy.split(":")
+        cursor = cnxn.cursor()
+        #        print("SELECT ID FROM proxies where ipv4 ='" + ip2int(ip) + "' and port = '" + port + "'")
+        cursor.execute("SELECT ID FROM proxies where ipv4 ='" + ip2int(ip) + "' and port = '" + port + "'")
+        row_count = cursor.rowcount
+        if row_count == 0:
+            #			cursor.execute("INSERT INTO proxies (ipv4,port) VALUES ('"+ ip2int(ip) +"','"+ port +"')")
+            #			cnxn.commit()
+            return False
+        else:
+            return True
+    except Exception as ex:
+        logging.exception(ex)
 
-	if os.path.exists(output_bad):
-		oldsize= len(z)
-		f2 = open(output_bad, "r")
-		for x2 in f2:
-			z.append(x2.split("--")[0])
-		f2.close()
-		logging.info("Loading " +  str(len(z)-oldsize) + " bad elements already tested")
 
-	for elem in proxies:
-		if (elem["ip"]+":"+elem["port"]) not in z:
-			inq.put(elem["ip"]+":"+elem["port"])
-	inq.put(sentinel)
+def parse_results(file, inq,sizeq):
+    #pbar = tqdm(total=sizeq, desc='Processing queue')
+    #bar = progressbar.ProgressBar(max_value=sizeq, prefix='Queue size: ')
+    #pbar2 = tqdm(total=sizeq)
+    bar = progressbar.ProgressBar(max_value=sizeq, prefix='Elements in queue: ',total=50000000).start()
+    logging.info("Reading " + file)
+    f = open(file, "r")
+    nb_proxies = 0;
+    for x in f:
+        if "#" not in x:
+            y = x.split()
+            if len(y) == 5:
+                port = y[2]
+                ip = y[3]
+                if not already_in_db(ip + ":" + port):
+                    inq.put(ip + ":" + port)
+                    nb_proxies += 1
+                    #pbar.update(nb_proxies)
+                    bar.update(inq.qsize())
 
-def process_inq(inq,website,timeout,output_good,output_bad,ignore):
-	for x in iter(inq.get, sentinel):
-		Status, Result = test_proxies(x,website,timeout,ignore)
-		result=str(Result)
-		if(Status):
-			logging.warning(x + " -- " + result)
-			save(x +"--" + result,output_good)
-		else:
-			if "Login required" in result or "Content altered" in result:
-				logging.warning(x + " -- " + result)
-			else:
-				logging.info(x + " -- " + result)
-			save(x +"--" + result,output_bad)
 
-	logging.warning("Processing queue empty!")
+    inq.put(sentinel)
+    logging.info(str(nb_proxies) + " proxies loaded from file")
+    return
 
-def read(proxies_uniq,inq):
-	for x in proxies_uniq:
-		inq.put(x["ip"]+":"+x["port"])
-	inq.put(sentinel)
+def fingerprint(website, TIMEOUT):
+    try:
+        req = urlrequest.Request(website)
+        req.addheaders = [(UA)]
+        response = urlrequest.urlopen(req, timeout=TIMEOUT)
+        m = hashlib.md5()
+        content = response.read()
+        page_body=content
+        m.update(content)
+        md5_hash = m.hexdigest()
+        logging.WARNING("Hash value of the content of " + website + " : " + md5_hash)
+        return md5_hash
+    except Exception as e:
+        logging.error(e)
+        logging.error("Cannot fetch the website used to compare integrity!")
+        exit(0)
+
+
+def test_proxies(proxy, website, TIMEOUT, ignore):
+    time.sleep(0.02)
+    return False, "test"
+    try:
+        req = urlrequest.Request(website)
+        req.set_proxy(proxy, 'http')
+        req.addheaders = [(UA)]
+        response = urlrequest.urlopen(req, timeout=TIMEOUT)
+    except ConnectionRefusedError:
+        return False, "ConnectionRefusedError"
+    except ConnectionResetError:
+        return False, "Connection reset"
+    except http.client.BadStatusLine:
+        return False, "Bad status"
+    except IOError as a:
+        if hasattr(a, 'code'):
+            return False, a.code
+        if hasattr(a, 'reason'):
+            return False, a.reason
+        else:
+            return False, a
+    except urllib.error.URLError as z:
+        if hasattr(z, 'code'):
+            return False, z.code
+        if hasattr(z, 'reason'):
+            return False, z.reason
+        else:
+            return False, z
+    else:
+        if ignore is None:
+            return True, str(response.getcode())
+
+        m = hashlib.md5()
+        content = response.read()
+        m.update(content)
+        if m.hexdigest() != MD5_SUM:
+            if page_body in content:
+                return True, str(response.getcode()) + " Content altered"
+            elif "login".encode() in content or "authorization".encode() in content:
+                return False, str(response.getcode()) + " Login required"
+            else:
+                return False, str(response.getcode()) + " Content unknown"
+        else:
+            return True, str(response.getcode()) + " Integrity check OK"
+
+
+def process_inq(inq, website, timeout, ignore):
+    for x in iter(inq.get, sentinel):
+        Status, Result = test_proxies(x, website, timeout, ignore)
+        result = str(Result)
+        update_db_result(x, result)
+        if (Status):
+            logging.warning(x + " -- " + result)
+        else:
+            logging.info(x + " -- " + result)
+
+    logging.warning("Processing queue empty!")
+
 
 def main():
-	output_good = os.path.join(os.path.dirname(os.path.abspath(__file__)),"good.txt")
-	output_bad =  os.path.join(os.path.dirname(os.path.abspath(__file__)),"bad.txt")
-	parser = OptionParser(usage="usage: %prog [options]")
+    parser = OptionParser(usage="usage: %prog [options]")
 
-	parser.add_option("-m", "--masscan",
-					  default="/root/masscan/data/out.txt",action="store", type="string", dest="masscan_results",nargs=1,
-					  help="Specify the file containing Masscan's results. Default: /root/masscan/data/out.txt")
-	parser.add_option("-b", "--bad",
-					  action="store", type="string", dest="output_bad",nargs=1,
-					  help="(Optional) Specify the output file for the proxies that aren't working. Default: "+ output_bad)
-	parser.add_option("-g", "--good",
-					  action="store", type="string", dest="output_good",nargs=1,
-					  help="(Optional) Specify the output file for the working proxies. Default: " +output_good)
-	parser.add_option("-w", "--website",
-					  default="http://perdu.com",action="store", type="string", dest="website",nargs=1,
-					  help="(Optional) Specify the website used to test the proxies. Default: http://perdu.com")
-	parser.add_option("-p", "--thread",
-					  default=10,action="store", type="int", dest="THREADS",nargs=1,
-					  help="(Optional) Specify the number of threads used to test the proxies. Default: 10")
-	parser.add_option("-t", "--timeout",
-					  default=6,action="store", type="int", dest="timeout",nargs=1,
-					  help="(Optional) Specify the timeout period when testing a proxy. Default: 6")
-	parser.add_option("-q", "--queue",
-					  default=10000,action="store", type="int", dest="QUEUE_SIZE",nargs=1,
-					  help="(Optional) Specify the size of the queue. Default: 10000")
-	parser.add_option("-i", "--ignore",
-					  dest="ignore",nargs=0,
-					  help="(Optional) Ignore integrity validation of returned content")
+    parser.add_option("-m", "--masscan",
+                      default="/root/masscan/data/out.txt", action="store", type="string", dest="masscan_results",
+                      nargs=1,
+                      help="Specify the file containing Masscan's results. Default: /root/masscan/data/out.txt")
+    parser.add_option("-w", "--website",
+                      default="http://perdu.com", action="store", type="string", dest="website", nargs=1,
+                      help="(Optional) Specify the website used to test the proxies. Default: http://perdu.com")
+    parser.add_option("-p", "--thread",
+                      default=10, action="store", type="int", dest="THREADS", nargs=1,
+                      help="(Optional) Specify the number of threads used to test the proxies. Default: 10")
+    parser.add_option("-t", "--timeout",
+                      default=6, action="store", type="int", dest="timeout", nargs=1,
+                      help="(Optional) Specify the timeout period when testing a proxy. Default: 6")
+    parser.add_option("-q", "--queue",
+                      default=10000, action="store", type="int", dest="QUEUE_SIZE", nargs=1,
+                      help="(Optional) Specify the size of the queue. Default: 10000")
+    parser.add_option("-i", "--ignore",
+                      dest="ignore", nargs=0,
+                      help="(Optional) Ignore integrity validation of returned content")
 
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-	logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-	if not os.path.isfile(options.masscan_results):
-		logging.error("Masscan results cannot be read!")
-		parser.print_help()
-		exit(0)
-	else:
-		proxies = parse_results(options.masscan_results)
+    if not os.path.isfile(options.masscan_results):
+        logging.error("Masscan results cannot be read!")
+        parser.print_help()
+        exit(0)
 
-	if options.website is None:
-		options.website="http://perdu.com"
+    if options.website is None:
+        options.website = "http://perdu.com"
 
-	if options.output_bad is not None:
-		logging.info("Setting output file for not working proxy to: " + options.output_bad)
-		output_bad=options.output_bad
-	else:
-		logging.info("Default output file for bad proxies selected: " + output_bad)
+    if options.ignore is None:
+        MD5_SUM = fingerprint(options.website, options.timeout)
+    else:
+        logging.info("Skipping integrity validation")
 
-	if options.output_good is not None:
-		logging.info("Setting output file for working proxy to: " + options.output_good)
-		output_good=options.output_good
-	else:
-		logging.info("Default output file for good proxies selected: " + output_good)
+    inq = queue.Queue(maxsize=options.QUEUE_SIZE)
+    threading.Thread(target=parse_results, args=(options.masscan_results, inq,options.QUEUE_SIZE)).start()
 
-	if options.ignore is None:
-		MD5_SUM=fingerprint(options.website,options.timeout)
-	else:
-		logging.info("Skipping integrity validation")
+    logging.warning("Starting " + str(options.THREADS) + " threads for processing\n "
+                                                         "**********************************************")
+    for i in range(options.THREADS):
+        threading.Thread(target=process_inq, args=(inq, options.website, options.timeout, options.ignore)).start()
 
-	inq = queue.Queue(maxsize=options.QUEUE_SIZE)
-	threading.Thread(target=load_and_remove_dupp, args=(inq,proxies,output_good,output_bad)).start()
-
-	logging.warning("Starting " + str(options.THREADS) + " threads for processing\n "
-														 "**********************************************")
-	for i in range(options.THREADS):
-		threading.Thread(target=process_inq, args=(inq,options.website,options.timeout,output_good,output_bad,options.ignore)).start()
+  #  pbar = tqdm(total=options.QUEUE_SIZE, desc='Processing queue')
+  #  while True:
+   #     time.sleep(0.01)
+    #    pbar.update(inq.qsize())
+ #   with tqdm(total=options.QUEUE_SIZE) as pbar:
+  #      while True:
+   #         cur_perc = inq.qsize()
+    #        pbar.update(inq.qsize()-pbar.n)  # here we update the bar of increase of cur_perc
+     #       if cur_perc == options.QUEUE_SIZE:
+      #          break
+   # bar = progressbar.ProgressBar(max_value=options.QUEUE_SIZE)
+    #while True:
+     #   time.sleep(0.1)
+      #  bar.update(inq.qsize())
 
 if __name__ == '__main__':
-	main()
+    main()
